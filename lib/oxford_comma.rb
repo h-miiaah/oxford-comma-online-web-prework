bafcddb5bba533b2f8a24576cab41a232af21e77
Haim D <haim11293@aol.com>
2019-06-28 01:00:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.size == 1
     return array[0]
   elsif array.size == 2
-    return array.join(" and ")
+    return Array.join(" and ")
   else
-    return array(0...-1).join(", ") << ", and #{array[-1]}"
+    return Array(0...-1).join(", ") << ", and #{array[-1]}"
   end
 end
